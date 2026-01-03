0a6ceba2067d80f31da10a9b6d703e1631d42556
root <root@hydra-3.lyon.grid5000.fr>
2026-01-03 22:02:54 +0100

    Fix: Correct matrix multiplication indices

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def multiply_matrices(A, B):
     for i in range(m):
         for j in range(p):
             total = 0
-            for k in range(len(B)):  # should be len(A[0])
+            for k in range(len(A[0])):  # should be len(A[0])
                 # Intentional bug: wrong indices
                 total += A[k][i] * B[j][k]
             C[i][j] = total
