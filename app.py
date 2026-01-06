de2e479e7f3422f3dc4c932774f79ed065a49e9b
root <root@chuc-3.lille.grid5000.fr>
2026-01-06 02:14:37 +0100

    Fix matrix multiplication bug in app.py

--- app.py
+++ app.py
@@ -17,9 +17,8 @@ def multiply_matrices(A, B):
     for i in range(m):
         for j in range(p):
             total = 0
-            for k in range(len(B)):  # should be len(A[0])
-                # Intentional bug: wrong indices
-                total += A[k][i] * B[j][k]
+            for k in range(len(A[0])):
+                total += A[i][k] * B[k][j]
             C[i][j] = total
 
     return C
