7345d80eee5348d1e12087c19c7a892f21c07203
root <root@hydra-1.lyon.grid5000.fr>
2026-01-15 22:32:47 +0100

    Fix matrix multiplication indices

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ def multiply_matrices(A, B):
     for i in range(m):
         for j in range(p):
             total = 0
-            for k in range(len(A)):
+            for k in range(len(A[0])):
                 
-                total += A[k][i] * B[j][k]
+                total += A[i][k] * B[k][j]
             C[i][j] = total
 
     return C
