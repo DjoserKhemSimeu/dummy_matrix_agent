14dc67d3c3165f8f118f49a0888abd9ba7b3680a
Djoser SIMEU <97344225+DjoserKhemSimeu@users.noreply.github.com>
2026-01-17 22:41:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def multiply_matrices(A, B):
     for i in range(m):
         for j in range(p):
             total = 0
-            for k in range(len(A)):
+            for k in range(len(B)):
                 
                 total += A[k][i] * B[j][k]
             C[i][j] = total
